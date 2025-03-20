03a968e29a8f7fbca5f39aff98179c15e9f1ff7b
jacksoom <lifengliu1994@gmail.com>
2025-03-20 15:42:57 +0800

    feat: add avsi base error (#18)
    * feat: add avsi base error
    
    * lint imports
    
    * fix ci lint

---
+++ types/errors.go
@@ -0,0 +1,34 @@
+package types
+
+import (
+	avsitypes "github.com/0xPellNetwork/pelldvs/avsi/types"
+
+	sdkerrors "github.com/0xPellNetwork/pellapp-sdk/errors"
+)
+
+// AvsiBaseError contains common fields for AVSI error responses
+type AvsiBaseError struct {
+	Code      uint32
+	Log       string
+	Events    []avsitypes.Event
+	Codespace string
+}
+
+// WarpAvsiBaseError creates a standardized error structure from an error and result
+func WarpAvsiBaseError(err error, res *AvsiResult, debug bool) *AvsiBaseError {
+	// Extract error details
+	space, code, log := sdkerrors.AVSIInfo(err, debug)
+
+	// Handle potentially nil result
+	var events []avsitypes.Event
+	if res != nil {
+		events = res.Events
+	}
+
+	return &AvsiBaseError{
+		Code:      code,
+		Log:       log,
+		Events:    events,
+		Codespace: space,
+	}
+}
